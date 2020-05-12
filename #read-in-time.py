a3fb855686d30ace86f54e34360501a269079ee0
Agent Fanci <ahflintgruber@knox.edu>
2020-05-12 00:22:12 -0500

    First musings and research

---
+++ #read-in-time.py
@@ -0,0 +1,22 @@
+#read-in-time
+import csv
+
+filename = 
+
+
+class messageReader {
+    with open(filename, 'r') as csvfile: 
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile) 
+      
+    # extracting field names through first row 
+    fields = next(csvreader) 
+  
+    # extracting each data row one by one 
+    for row in csvreader: 
+        rows.append(row) 
+
+
+
+
+}
