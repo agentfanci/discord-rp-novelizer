ded7528daf014001129187dda001cf60db818c26
Agent Fanci <ahflintgruber@knox.edu>
2020-05-12 21:50:21 -0500

    added add_tag, beginning to consider dates

--- Message.py
+++ Message.py
@@ -1,3 +1,7 @@
+import datetime
+
+
+
 class Message:
    
 
@@ -5,8 +9,19 @@ class Message:
         self.author = auth
         self.date = dt
         self.content = cnt
-        self.tags = {}
+        self.tags = {"all"}
         self.channel = fil
         self.attachments = attatch
         self.reactions = rxns
+    
+    def add_tag(self, tag):
+        self.tags.add(tag)
+    
+    def to_24_time_and_date(self, dt):
+        #format: "23-May-19 11:26 AM"
+        #so how to convert
+        date, time, merd = self.dt.split(" ")
+
+
+
     
